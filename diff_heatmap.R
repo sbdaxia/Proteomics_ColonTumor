54612b9ccffb4e1ef6192da00696bca570f1f978
Bing <shuibings@gmail.com>
2021-05-27 19:58:43 -0400

    Casein kinase target phosphorylation exploration

--- diff_heatmap.R
+++ diff_heatmap.R
@@ -1,38 +1,29 @@
-diff_id <- c()
+library(tidyverse)
 
-for (i in 1:dim(crcMS_diff)[1]) {
-  if (crcMS_diff$p_values[i] < 0.05 & crcMS_diff$q_values[i] < 0.1) {
-    diff_id <- c(diff_id, crcMS_diff$X1[i])
-  }
-}
+crcMS_diff <- read.csv("crcMS_diff.csv")
+crcMS <- read.csv("2017-03-19_Haigis_5v5_Pro.csv")
 
-diff_index <- c()
-
-for (i in 1: length(diff_id)) {
-  exactname <- paste('^',diff_id[i], '$', sep = '')
-  diff_index <- c(diff_index, grep(exactname, protein_quant$Protein.Id))
-}
-
-diff_raw_quant <- protein_quant[diff_index,]
-library(mosaic)
-for (i in 1:dim(diff_raw_quant)[1]) {
-  diff_raw_quant[i, 3:12]<- zscore(as.numeric(as.character(diff_raw_quant[i, 3:12])))
-}
-
+diff_protein <- crcMS_diff %>% filter(p_values < 0.05 & q_values < 0.1) %>% select(Protein.Id) %>% unlist()
+diff_raw_quant <- crcMS %>% filter(Protein.Id %in% diff_protein)
 
 diff_raw_quant_matrix <- as.matrix(diff_raw_quant[, -c(1:2)])
 rownames(diff_raw_quant_matrix) <- diff_raw_quant$Protein.Id
 
 library(gplots)
 library(RColorBrewer)
+library(mosaic)
 
-my_palette <- colorRampPalette(c("red", "white", "blue"))(1000)
+for (i in 1:dim(diff_raw_quant_matrix)[1]) {
+  diff_raw_quant_matrix[i,1:10] <- zscore(as.numeric(diff_raw_quant_matrix[i,1:10]))
+}
 
-png('Diff_crc_proteomics_heatmap.png',
-    width = 600,
-    height = 1400,
-    res = 200,
-    pointsize = 8)
+
+# all DE proteins
+my_palette <- colorRampPalette(c("blue", "white", "red"))(256)
+
+pdf('Diff_crc_proteomics_heatmap.pdf',
+    width = 6,
+    height = 12)
 heatmap.2(diff_raw_quant_matrix,
           main = "Differentially expressed\nin colon tumor\np < 0.05 and q < 0.1",
           density.info = "none",
@@ -46,6 +37,60 @@ heatmap.2(diff_raw_quant_matrix,
           dendrogram = "row",
           labRow = FALSE,
           ylab = "Proteins",
+          cexCol = 2,
+          Colv = "NA")
+dev.off()
+
+# Top 10 up/down proteins
+top10diff <- crcMS_diff %>% filter(p_values < 0.05 & q_values < 0.1)
+order_list <- top10diff[order(top10diff$LFC, decreasing = TRUE), ]
+top10up <-  order_list[1:10, ]
+top10down <- order_list[2462:2471, ]
+top10 <- rbind(top10up, top10down)
+
+top10_quant <- crcMS %>% filter(Protein.Id %in% top10$Protein.Id)
+
+top10_quant_matrix <- as.matrix(top10_quant[, -c(1:2)])
+
+# ID conversion
+library(org.Mm.eg.db)
+library(ensembldb)
+library(AnnotationDbi)
+library(EnsDb.Mmusculus.v79)
+
+annotations_edb <- AnnotationDbi::select(org.Mm.eg.db,
+                                         keys = top10_quant$Protein.Id,
+                                         columns = c("ENSEMBL", "SYMBOL"),
+                                         keytype = "UNIPROT")
+
+# manual fill-in
+annotations_edb$SYMBOL[c(14,16,19)] <- c("Ig.heavy.chain.V region.36-60", "Ig.heavy.chain.V.region.441", "Ly6g")
+
+top10_quant <- inner_join(top10_quant, annotations_edb[,c(1,3)], by = c("Protein.Id" = "UNIPROT"))
+
+rownames(top10_quant_matrix) <- top10_quant$SYMBOL
+
+for (i in 1:dim(top10_quant_matrix)[1]) {
+  top10_quant_matrix[i,1:10] <- zscore(as.numeric(top10_quant_matrix[i,1:10]))
+}
+
+
+# Top 10 DE proteins
+pdf('Top10Diff_crc_proteomics_heatmap.pdf',
+    width = 10,
+    height = 10)
+heatmap.2(top10_quant_matrix,
+          main = "Top 10 Differentially expressed\nin colon tumor\np < 0.05 and q < 0.1",
+          density.info = "none",
+          key = TRUE,
+          lwid = c(3,7),
+          lhei = c(1,7),
+          col=my_palette,
+          margins = c(8,14),
+          symbreaks = TRUE,
+          trace = "none",
+          dendrogram = "row",
+          labRow = rownames(top10_quant_matrix),
           cexCol = 2,
           Colv = "NA")
 dev.off()
